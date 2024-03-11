c57b17076edf53b96fb2f79a3096806fbc26c9f1
mohiudd1n <moinuddinmerchant94@gmail.com>
2024-03-12 00:21:07 +0530

    Updated Project

--- app/src/main/java/com/example/madmp1/frag1.java
+++ app/src/main/java/com/example/madmp1/frag1.java
@@ -6,6 +6,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 
 import androidx.fragment.app.Fragment;
 
@@ -19,71 +20,71 @@ public class frag1 extends Fragment {
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 
-    Button caracc,gymacc,clgacc,sprtacc;
+    ImageView caracc,gymacc,clgacc,sprtacc;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-            View view = inflater.inflate(R.layout.fragment_frag1, container, false);
+        View view = inflater.inflate(R.layout.fragment_frag1, container, false);
 
-            // Initialize your objects here
-            caracc = view.findViewById(R.id.caracc);
-            gymacc = view.findViewById(R.id.gymacc);
-            clgacc = view.findViewById(R.id.college_acc);
-            sprtacc = view.findViewById(R.id.sports_acc);
+        // Initialize your objects here
+        caracc = view.findViewById(R.id.caracc);
+        gymacc = view.findViewById(R.id.gymacc);
+        clgacc = view.findViewById(R.id.college_acc);
+        sprtacc = view.findViewById(R.id.sports_acc);
 
-            ImageSlider imageSlider = view.findViewById(R.id.imageslider);
-            ArrayList<SlideModel> slideModels = new ArrayList<>();
+        ImageSlider imageSlider = view.findViewById(R.id.imageslider);
+        ArrayList<SlideModel> slideModels = new ArrayList<>();
 
-            slideModels.add(new SlideModel(R.drawable.offer1, ScaleTypes.FIT));
-            slideModels.add(new SlideModel(R.drawable.offer2, ScaleTypes.FIT));
-            slideModels.add(new SlideModel(R.drawable.offer3, ScaleTypes.FIT));
-            slideModels.add(new SlideModel(R.drawable.offer4, ScaleTypes.FIT));
-            slideModels.add(new SlideModel(R.drawable.offer5, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.offer1, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.offer2, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.offer3, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.offer4, ScaleTypes.FIT));
+        slideModels.add(new SlideModel(R.drawable.offer5, ScaleTypes.FIT));
 
-            imageSlider.setImageList(slideModels,ScaleTypes.FIT);
+        imageSlider.setImageList(slideModels,ScaleTypes.FIT);
 
-            // Set a click listener on the button
-            caracc.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Handle button click
-                    startActivity(new Intent(v.getContext(),car_accessories.class));
-                }
-            });
+        // Set a click listener on the button
+        caracc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle button click
+                startActivity(new Intent(v.getContext(),car_accessories.class));
+            }
+        });
 
-            gymacc.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Handle button click
-                    startActivity(new Intent(v.getContext(),gym_accessories.class));
-                }
-            });
+        gymacc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle button click
+                startActivity(new Intent(v.getContext(),gym_accessories.class));
+            }
+        });
 
-            clgacc.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Handle button click
-                    startActivity(new Intent(v.getContext(),college_accessory.class));
-                }
-            });
+        clgacc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle button click
+                startActivity(new Intent(v.getContext(),college_accessory.class));
+            }
+        });
 
-            sprtacc.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    // Handle button click
-                    startActivity(new Intent(v.getContext(), sports_accessory.class));
-                }
-            });
+        sprtacc.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Handle button click
+                startActivity(new Intent(v.getContext(), sports_accessory.class));
+            }
+        });
 
 
 
-            // Other setup tasks can go here
+        // Other setup tasks can go here
 
-            return view;
-        }
-
-        // return inflater.inflate(R.layout.fragment_frag1, container, false);
-
+        return view;
     }
+
+    // return inflater.inflate(R.layout.fragment_frag1, container, false);
+
+}
